37a684877aca1f1309259ab5e82119d38b6bc453
Ronoaldo JLP <ronoaldo@gmail.com>
2021-09-23 22:52:46 -0300

    Added backup support to Cloud Storage.

--- backup.sh
+++ backup.sh
@@ -7,8 +7,15 @@ BASENAME=`basename $BASEDIR`
 BACKUP_DIR=$HOME/backups/$(date +'%Y%m')
 BACKUP_FILE=$BACKUP_DIR/$BASENAME-$(date +'%Y%m%d-%H%M%S').tar.gz
 
+echo "Saving backup to $BACKUP_FILE ..."
 cd $BASEDIR
+. .env
 mkdir -p $BACKUP_DIR
 docker-compose exec -T db pg_dump -c -U mercurio > .minetest/db.sql
 tar --exclude=mapserver.tiles -cvzf $BACKUP_FILE .minetest/db.sql .minetest/world
 rm -f .minetest/db.sql
+
+if [ x$MINETEST_BACKUP_STORAGE = x"true" ] ; then
+    echo "Moving backup to Cloud Storage ... "
+    gsutil -m mv $BACKUP_FILE gs://minetest-hosting/servers/mercurio/backups/
+fi
