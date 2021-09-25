bf9443c63ef3eebe4111d65fc7c3e014503e1183
Ronoaldo JLP <ronoaldo@gmail.com>
2021-09-25 18:03:01 -0500

    Fixed backup script.

--- backup.sh
+++ backup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
-set -x
 
 BASEDIR=`readlink -f $(dirname $0)`
 BASENAME=`basename $BASEDIR`
 BACKUP_DIR=$HOME/backups/$(date +'%Y%m')
 BACKUP_FILE=$BACKUP_DIR/$BASENAME-$(date +'%Y%m%d-%H%M%S').tar.gz
+
+# Ensure we clean the temp db.sql file which is HUGE
+_cleanup() {
+    echo "Cleaning up ..."
+    cd $BASEDIR
+    rm -vf .minetest/db.sql
+}
+
+trap "_cleanup" EXIT KILL
 
 echo "Saving backup to $BACKUP_FILE ..."
 cd $BASEDIR
@@ -13,9 +21,11 @@ cd $BASEDIR
 mkdir -p $BACKUP_DIR
 docker-compose exec -T db pg_dump -c -U mercurio > .minetest/db.sql
 tar --exclude=mapserver.tiles -cvzf $BACKUP_FILE .minetest/db.sql .minetest/world
-rm -f .minetest/db.sql
+rm -vf .minetest/db.sql
 
-if [ x$MINETEST_BACKUP_STORAGE = x"true" ] ; then
+if [ x$MINETEST_BACKUP_GCS = x"true" ] ; then
     echo "Moving backup to Cloud Storage ... "
     gsutil -m mv $BACKUP_FILE gs://minetest-hosting/servers/mercurio/backups/${BASENAME}.current.tar.gz
+else
+    echo "Not moving to Cloud Storage. MINETEST_BACKUP_GCS env is not set to 'true'."
 fi
