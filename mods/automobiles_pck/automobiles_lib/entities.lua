f7140514db3a451fa43afc16e8aff0e462ecadb5
Ronoaldo JLP <ronoaldo@gmail.com>
2023-09-22 17:36:00 -0300

    mods: updated automobiles-pck mod

---
+++ mods/automobiles_pck/automobiles_lib/entities.lua
@@ -0,0 +1,133 @@
+
+function automobiles_lib.on_rightclick (self, clicker)
+	if not clicker or not clicker:is_player() then
+		--return
+	end
+
+	local name = clicker:get_player_name()
+    --[[if self.owner and self.owner ~= name and self.owner ~= "" then return end]]--
+    if self.owner == "" then
+        self.owner = name
+    end
+    
+	if name == self.driver_name then
+        local formspec_f = automobiles_lib.driver_formspec
+        if self._formspec_function then formspec_f = self._formspec_function end
+        formspec_f(name)
+	else
+        if name == self.owner or
+            (self.driver_name == nil and
+            ( minetest.check_player_privs(clicker, "valet_parking") or minetest.check_player_privs(clicker, {server=true}) )
+            ) then
+            if clicker:get_player_control().aux1 == true then
+                automobiles_lib.show_vehicle_trunk_formspec(self, clicker, self._trunk_slots)
+            else
+                --is the owner, okay, lets attach
+                local attach_driver_f = automobiles_lib.attach_driver
+                if self._attach then attach_driver_f = self._attach end
+                attach_driver_f(self, clicker)
+                -- sound
+                local base_pitch = 1
+                if self._base_pitch then base_pitch = self._base_pitch end
+                self.sound_handle = minetest.sound_play({name = self._engine_sound},
+                        {object = self.object, gain = 1, pitch = base_pitch, max_hear_distance = 30, loop = true,})
+            end
+        else
+            --minetest.chat_send_all("clicou")
+            --a passenger
+            if self._passenger == nil then
+                --there is no passenger, so lets attach
+                if self.driver_name then
+                    local attach_pax_f = automobiles_lib.attach_pax
+                    if self._attach_pax then attach_pax_f = self._attach_pax end
+                    attach_pax_f(self, clicker, true)
+                end
+            else
+                --there is a passeger
+                if self._passenger == name then
+                    --if you are the psenger, so deattach
+                    local dettach_pax_f = automobiles_lib.dettach_pax
+                    if self._dettach_pax then dettach_pax_f = self._dettach_pax end
+                    dettach_pax_f(self, clicker)
+                end
+            end
+        end
+	end
+end
+
+function automobiles_lib.on_punch (self, puncher, ttime, toolcaps, dir, damage)
+	if not puncher or not puncher:is_player() then
+		return
+	end
+
+	local name = puncher:get_player_name()
+    --[[if self.owner and self.owner ~= name and self.owner ~= "" then return end]]--
+    if self.owner == nil then
+        self.owner = name
+    end
+    	
+    if self.driver_name and self.driver_name ~= name then
+		-- do not allow other players to remove the object while there is a driver
+		return
+	end
+    
+    local is_attached = false
+    if puncher:get_attach() == self.driver_seat then is_attached = true end
+
+    local itmstck=puncher:get_wielded_item()
+    local item_name = ""
+    if itmstck then item_name = itmstck:get_name() end
+
+    --refuel procedure
+    --[[
+    refuel works it car is stopped and engine is off
+    ]]--
+    local velocity = self.object:get_velocity()
+    local speed = automobiles_lib.get_hipotenuse_value(vector.new(), velocity)
+    if math.abs(speed) <= 0.1 then
+        if automobiles_lib.loadFuel(self, puncher:get_player_name(), false, self._max_fuel) then return end
+    end
+    -- end refuel
+
+    if is_attached == false then
+
+        -- deal with painting or destroying
+	    if itmstck then
+            --race status restart
+            if item_name == "checkpoints:status_restarter" and self._engine_running == false then
+                --restart race current status
+                self._last_checkpoint = ""
+                self._total_laps = -1
+                self._race_id = ""
+                return
+            end
+
+            local paint_f = automobiles_lib.set_paint
+            if self._painting_function then paint_f = self._painting_function end
+            if paint_f(self, puncher, itmstck) == false then
+                local is_admin = false
+                is_admin = minetest.check_player_privs(puncher, {server=true})
+                --minetest.chat_send_all('owner '.. self.owner ..' - name '.. name)
+			    if not self.driver and (self.owner == name or is_admin == true) and toolcaps and
+                        toolcaps.damage_groups and toolcaps.damage_groups.fleshy then
+                    self.hp = self.hp - 10
+                    minetest.sound_play("automobiles_collision", {
+                        object = self.object,
+                        max_hear_distance = 5,
+                        gain = 1.0,
+                        fade = 0.0,
+                        pitch = 1.0,
+                    })
+			    end
+		    end
+        end
+
+        if self.hp <= 0 then
+            local destroy_f = automobiles_lib.destroy
+            if self._destroy_function then destroy_f = self._destroy_function end
+            destroy_f(self)
+        end
+
+    end
+    
+end
--- mods/automobiles_pck/automobiles_lib/init.lua
+++ mods/automobiles_pck/automobiles_lib/init.lua
@@ -3,12 +3,24 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 automobiles_lib = {}
 
+local storage = minetest.get_mod_storage()
+
 automobiles_lib.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,
                 ['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 10,
                 ['airutils:biofuel'] = 1,}
 
 automobiles_lib.gravity = 9.8
 automobiles_lib.is_creative = minetest.settings:get_bool("creative_mode", false)
+
+
+local load_noob_mode = storage:get_int("noob_mode")
+automobiles_lib.noob_mode = false
+automobiles_lib.extra_stepheight = 0
+-- 1 == true ---- 2 == false
+if load_noob_mode == 1 then
+    automobiles_lib.load_noob_mode = true
+    automobiles_lib.extra_stepheight = 1
+end
 
 --cars colors
 automobiles_lib.colors ={
@@ -355,6 +367,88 @@ function automobiles_lib.put_light(self)
 
 end
 
+function automobiles_lib.destroy(self, puncher)
+    automobiles_lib.remove_light(self)
+    if self.sound_handle then
+        minetest.sound_stop(self.sound_handle)
+        self.sound_handle = nil
+    end
+
+    if self.driver_name then
+        -- detach the driver first (puncher must be driver)
+        if puncher then
+            puncher:set_detach()
+            puncher:set_eye_offset({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
+            if minetest.global_exists("player_api") then
+                player_api.player_attached[self.driver_name] = nil
+                -- player should stand again
+                player_api.set_animation(puncher, "stand")
+            end
+        end
+        self.driver_name = nil
+    end
+
+    local pos = self.object:get_pos()
+
+    if self.front_suspension then self.front_suspension:remove() end
+    if self.lf_wheel then self.lf_wheel:remove() end
+    if self.rf_wheel then self.rf_wheel:remove() end
+    if self.rear_suspension then self.rear_suspension:remove() end
+    if self.lr_wheel then self.lr_wheel:remove() end
+    if self.rr_wheel then self.rr_wheel:remove() end
+    if self.steering then self.steering:remove() end
+    if self.steering_axis then self.steering_axis:remove() end
+    if self.driver_seat then self.driver_seat:remove() end
+    if self.passenger_seat then self.passenger_seat:remove() end
+    if self.fuel_gauge then self.fuel_gauge:remove() end
+    if self.lights then self.lights:remove() end
+    if self.r_lights then self.r_lights:remove() end
+    if self.reverse_lights then self.reverse_lights:remove() end
+    if self.turn_l_light then self.turn_l_light:remove() end
+    if self.turn_r_light then self.turn_r_light:remove() end
+
+    automobiles_lib.destroy_inventory(self)
+    self.object:remove()
+
+    pos.y=pos.y+2
+
+    minetest.add_item({x=pos.x+math.random()-0.5,y=pos.y,z=pos.z+math.random()-0.5},'automobiles_lib:engine')
+    minetest.add_item({x=pos.x+math.random()-0.5,y=pos.y,z=pos.z+math.random()-0.5},'automobiles_lib:wheel')
+    minetest.add_item({x=pos.x+math.random()-0.5,y=pos.y,z=pos.z+math.random()-0.5},'automobiles_lib:wheel')
+    minetest.add_item({x=pos.x+math.random()-0.5,y=pos.y,z=pos.z+math.random()-0.5},'automobiles_lib:wheel')
+    minetest.add_item({x=pos.x+math.random()-0.5,y=pos.y,z=pos.z+math.random()-0.5},'automobiles_lib:wheel')
+end
+
+function automobiles_lib.engine_set_sound_and_animation(self, _longit_speed)
+    --minetest.chat_send_all('test1 ' .. dump(self._engine_running) )
+    if self.sound_handle then
+        if (math.abs(self._longit_speed) > math.abs(_longit_speed) + 0.03) or (math.abs(self._longit_speed) + 0.03 < math.abs(_longit_speed)) then
+            --minetest.chat_send_all('test2')
+            automobiles_lib.engineSoundPlay(self)
+        end
+    end
+end
+
+function automobiles_lib.engineSoundPlay(self)
+    --sound
+    if self.sound_handle then minetest.sound_stop(self.sound_handle) end
+    if self.object then
+        local base_pitch = 1
+        if self._base_pitch then base_pitch = self._base_pitch end
+
+        local snd_pitch = base_pitch + ((self._longit_speed/10)/2)
+        if self._transmission_state == 1 then
+            snd_pitch = 1 + (self._longit_speed/10)
+        end
+
+        self.sound_handle = minetest.sound_play({name = self._engine_sound},
+            {object = self.object, gain = 4,
+                pitch = snd_pitch,
+                max_hear_distance = 15,
+                loop = true,})
+    end
+end
+
 minetest.register_node("automobiles_lib:light", {
 	drawtype = "airlike",
 	--tile_images = {"automobiles_light.png"},
@@ -461,6 +555,8 @@ dofile(minetest.get_modpath("automobiles_lib") .. DIR_DELIM .. "fuel_management.
 dofile(minetest.get_modpath("automobiles_lib") .. DIR_DELIM .. "ground_detection.lua")
 dofile(minetest.get_modpath("automobiles_lib") .. DIR_DELIM .. "painter.lua")
 dofile(minetest.get_modpath("automobiles_lib") .. DIR_DELIM .. "inventory_management.lua")
+dofile(minetest.get_modpath("automobiles_lib") .. DIR_DELIM .. "formspecs.lua")
+dofile(minetest.get_modpath("automobiles_lib") .. DIR_DELIM .. "entities.lua")
 
 -- engine
 minetest.register_craftitem("automobiles_lib:engine",{
@@ -513,4 +609,64 @@ initial_properties = {
       return minetest.serialize(self.sdata)
     end,
 
+})
+
+minetest.register_privilege("valet_parking", {
+    description = "Gives a valet parking priv for a player",
+    give_to_singleplayer = true
+})
+
+minetest.register_chatcommand("transfer_vehicle", {
+    params = "<new_owner>",
+    description = "Transfer the property of a vehicle to another player",
+    privs = {interact=true},
+	func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        local target_player = minetest.get_player_by_name(param)
+        local attached_to = player:get_attach()
+
+		if attached_to ~= nil then
+            if target_player ~= nil then
+                local seat = attached_to:get_attach()
+                if seat ~= nil then
+                    local entity = seat:get_luaentity()
+                    if entity then
+                        if entity.owner == name or minetest.check_player_privs(name, {protection_bypass=true}) then
+                            entity.owner = param
+                            minetest.chat_send_player(name,core.colorize('#00ff00', " >>> This vehicle now is property of: "..param))
+                            automobiles_lib.setText(entity, "vehicle")
+                        else
+                            minetest.chat_send_player(name,core.colorize('#ff0000', " >>> only the owner or moderators can transfer this vehicle"))
+                        end
+                    end
+                end
+            else
+                minetest.chat_send_player(name,core.colorize('#ff0000', " >>> the target player must be logged in"))
+            end
+		else
+			minetest.chat_send_player(name,core.colorize('#ff0000', " >>> you are not inside a vehicle to perform the command"))
+		end
+	end
+})
+
+minetest.register_chatcommand("noobfy_the_vehicles", {
+    params = "<true/false>",
+    description = "Enable/disable the NOOB mode for the vehicles",
+    privs = {server=true},
+    func = function(name, param)
+        local command = param
+
+        if command == "false" then
+            automobiles_lib.noob_mode = false
+            automobiles_lib.extra_stepheight = 0
+            minetest.chat_send_player(name, ">>> Noob mode is disabled - A restart is required to changes take full effect")
+        else
+            automobiles_lib.noob_mode = true
+            automobiles_lib.extra_stepheight = 1
+            minetest.chat_send_player(name, ">>> Noob mode is enabled - A restart is required to changes take full effect")
+        end
+        local save = 2
+        if automobiles_lib.noob_mode == true then save = 1 end
+        storage:set_int("noob_mode", save)
+    end,
 })
